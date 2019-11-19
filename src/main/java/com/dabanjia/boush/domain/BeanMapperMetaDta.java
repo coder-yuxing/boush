191652bae8b1b1b6a8bfa9e45786d6848b0d048a
d-GuangRen <dingguangren@icloud.com>
2019-11-19 17:45:23 +0800

    Feat：定义beanMapper 模板，及所需元数据

--- src/main/java/com/dabanjia/boush/domain/BeanMapperMetaDta.java
+++ src/main/java/com/dabanjia/boush/domain/BeanMapperMetaDta.java
@@ -1,10 +1,29 @@
 package com.dabanjia.boush.domain;
 
+import lombok.Data;
+
 /**
  * Mapper 接口初始化所需元数据
  *
  * @author GuangRen
  * @date 2019/11/19
  */
+@Data
 public class BeanMapperMetaDta {
+
+    private String packageName;
+
+    private String beanClassName;
+
+    private String remarks;
+
+    private String author;
+
+    private String date;
+
+    private String name;
+
+    private String beanName;
+
+    private String beanParamName;
 }
